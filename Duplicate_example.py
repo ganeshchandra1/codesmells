d2f2d172a334abcc80d6de1e765811d7e4d62cf8
Ganesh Chandra <48551837+ganeshchandra1@users.noreply.github.com>
2023-10-30 15:50:17 -0700

    Update Duplicate_example.py

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -1,4 +1,14 @@
 #Duplicate_example
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+
+def calculate_perimeter(length, width):
+    perimeter = 2 * (length + width)
+    return perimeter
+
+
 def calculate_volume(length, width, height):
     volume = length * width * height
     return volume
